88c783c97302eecb437837910d0529b7ae5e6b1a
lifeng <lifeng@qq.com>
2018-05-21 01:26:56 +0800

    搜索提示

--- jiaxiao/js/main.js
+++ jiaxiao/js/main.js
@@ -10,12 +10,12 @@ $.searchDialog = function(option){
   '<form class="weui-search-bar__form">' +
   '<div class="weui-search-bar__box">' +
   '<i class="weui-icon-search"></i>' +
-  '<input type="search" class="weui-search-bar__input" id="searchInput" placeholder="搜索" required/>' +
+  '<input type="search" class="weui-search-bar__input" id="searchInput" placeholder="姓名/拼音/手机号" required/>' +
   '<a href="javascript:" class="weui-icon-clear" id="searchClear"></a>' +
   '</div>' +
   '<label class="weui-search-bar__label" id="searchText">' +
   '<i class="weui-icon-search"></i>' +
-  '<span>搜索</span>' +
+  '<span>姓名/拼音/手机号</span>' +
   '</label>' +
   '</form>' +
   '<a href="javascript:" class="weui-search-bar__cancel-btn" id="searchCancel">取消</a>' +
